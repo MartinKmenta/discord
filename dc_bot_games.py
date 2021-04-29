1d41bb20212ee15f059c7d98a67083a282ca407d
AdamVondracek <Adam_Vondracek@protonmail.com>
2021-04-29 15:40:59 +0200

    Update dc_bot_games.py

--- dc_bot_games.py
+++ dc_bot_games.py
@@ -3,92 +3,104 @@ from discord.ext import commands
 from discord.ext import tasks
 
 import requests
+from bs4 import BeautifulSoup
+
 
 
 class Games(commands.Cog):
-    def __init__(self):
-        #self.bot = bot
-        self.games_expiring = []
-        self.games_unknown_expiry = []
+    def __init__(self,bot):
+        self.bot = bot
+        self.data = {}
+        self.message = []
         
     def cog_unload(self):
         self.printer.cancel()
     
-    def __repr__(self):
-        return ""
-    
+    def __str__(self):
+        return "\n".join(self.message)
     
     @tasks.loop(hours = 12)
-    async def printer(self):
-        pass
+    async def printer(self,ctx):
+        self.find_games()
+        self.generate_message()
         
-    def printer_debug(self,message):
-        for x in message:
-            print(x)
+        for line in self.message:
+            print(line)
+            
+    def find_games(self):
+        # request page data
+        page = requests.get(url = "https://isthereanydeal.com/specials/#/filter:&giveaway")
     
-    def gamess(self):
-        r = requests.get(url = "https://isthereanydeal.com/specials/#/filter:&giveaway")
-
-        if r.status_code != 200:
-            print(f"Server returned {r.status_code}")
+        # handle errors
+        if page.status_code != 200:
+            print(f"Server returned {page.status_code}")
             return 
-
-        html = r.text[r.text.find('<div id=\'games\'>') : r.text.find('<div id="lazyload">')]
-        games = html.split("bundle-container-outer")
-        self.games_expiring = [ x for x in games if "left" in x ]
-        self.games_unknown_expiry = [ x for x in games if "unknown expiry" in x ]
+      
+        # load page into BeautifulSoup, find containers
+        soup = BeautifulSoup(page.content, 'html.parser')
+        div_games = soup.find(id="games")
+        games_container = div_games.find_all(class_ = "bundle-container")
         
-
-    def message_output(self):
-        # message is 
+        # store games in dictionary: keys = "steam","epic", "other"
+        data = {"steam":[],"epic":[],"other":[]}
+        template = {"title": None, "time": None, "url": None}
+        
+        # for all containers, find and write to data
+        for game in games_container:
+            # handle empty game, sholdn't occur
+            if game is None:
+                continue
+            
+            time = game.find(class_ = "bundle-time")
+            div_title = game.find(class_ = "bundle-title")
+            
+            # handle time not found
+            if time is None:
+                continue
+            
+            # handle title not found
+            if div_title is None:
+                continue
+            
+            a = div_title.select_one("a")
+            template["title"] = a.text
+            template["time"] = time.text
+            template["url"] = a["href"]
+            
+            if "https://store.steampowered.com/" in a["href"]:
+                key = "steam"
+            elif "https://www.epicgames.com/store/" in a["href"]:
+                key = "epic"
+            else:
+                key = "other"
+                
+            data[key].append(template.copy())
+            
+        self.data = data
+        
+    def generate_message(self):
         message = []
-        games_expiring_steam = [ x for x in self.games_expiring if "https://store.steampowered." in x ]
-        games_expiring_epic = [ x for x in self.games_expiring if "https://www.epicgames." in x ]
-        
-        if self.games_expiring == None or len(games_expiring_steam) == 0:
-            message.append("No games found :cry:")        
-
-        # steam games
-        if games_expiring_steam != None and len(games_expiring_steam) > 0:
-            message.append("Games on steam:")
-            for g in games_expiring_steam:
-                self.games_expiring.remove(g)
-                g = g[g.find('bundle-time') : ]
-                time_to_expire = g[g.find('>') + 1 : g.find('<')]
-                g = g[g.find('href') : ]
-                link = g[6 : g.find("class") - 2]
-                title = g[g.find('>') + 1 : g.find('<')]
-
-                message.append(f"{title}    {time_to_expire}    {link}")
-
-        message.append("----------------------------------------------------")
-
-        # epic games
-        if games_expiring_epic != None and len(games_expiring_epic) > 0:
-            message.append("Games on epic:")
-            for g in games_expiring_epic:
-                self.games_expiring.remove(g)
-                g = g[g.find('bundle-time') : ]
-                time_to_expire = g[g.find('>') + 1 : g.find('<')]
-                g = g[g.find('href') : ]
-                link = g[6 : g.find("class") - 2]
-                title = g[g.find('>') + 1 : g.find('<')]
-
-                message.append(f"{title}    {time_to_expire}    {link}")
-
-        message.append("----------------------------------------------------")
-
-        # other games
-        if self.games_expiring != None and len(self.games_expiring) > 0:
-            message.append("Other games:")
-            for g in self.games_expiring:
-                g = g[g.find('bundle-time') : ]
-                time_to_expire = g[g.find('>') + 1 : g.find('<')]
-                g = g[g.find('href') : ]
-                link = g[6 : g.find("class") - 2]
-                title = g[g.find('>') + 1 : g.find('<')]
-
-                message.append(f"{title}    {time_to_expire}    {link}")
-        
-        self.printer_debug(message)
-        return message
+        for key,games in self.data.items():
+            message.append(key)
+            message.append(30*"-")
+            for game in games:
+                message.append("{}\t{}\t{}".format(*game.values()))
+            message.append(30*"-")
+            
+        self.message = message
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
