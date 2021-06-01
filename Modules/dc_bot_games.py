4559b1636904ce57a20960a6ea73bf77a98d5548
AdamVondracek <Adam_Vondracek@protonmail.com>
2021-06-01 16:37:46 +0200

    Behavior fix
    Too many messages were sent.

--- Modules/dc_bot_games.py
+++ Modules/dc_bot_games.py
@@ -11,7 +11,7 @@ class Games(commands.Cog):
     """Inspired by https://discordpy.readthedocs.io/en/stable/ext/tasks/index.html,
     https://discordpy.readthedocs.io/en/stable/ext/commands/cogs.html"""
     
-    def __init__(self, bot, data):
+    def __init__(self, bot = None, data = None):
         self.bot = bot
         self.data = data
         self.games_data = {}
@@ -24,6 +24,7 @@ class Games(commands.Cog):
                       "humble bundle",
                       "other"
                       ]
+
 
     def cog_unload(self):
         self.printer.cancel()
@@ -46,8 +47,8 @@ class Games(commands.Cog):
             return
         
         # send in multiple messages
-        for line in self.message:
-            await default_channel.send(line)
+        for part in self.message:
+            await default_channel.send(part)
         
         # memory cleanup
         self.printer_is_done()
@@ -106,9 +107,10 @@ class Games(commands.Cog):
             
             # fill template
             a = div_title.select_one("a")
+            deal_url = a["href"]
+            
             template["title"] = a.text
             template["time"] = time.text
-            deal_url = a["href"]
             template["url"] = deal_url
             
             # group by domain
@@ -133,18 +135,35 @@ class Games(commands.Cog):
     
     def generate_message(self):
         # messages - list of string - stores readable lines
-        message = []
+        full_message = []
+        partial_message = ''
+        mlines = []
+        
         category_separator = 60*"-"
         
         # for all data, generate readable message.
         for key,games in self.games_data.items():
-            message.append(key)
-            message.append(category_separator)
+            mlines.append(category_separator)
+            mlines.append(key)
+            mlines.append(category_separator)
+                        
             for game in games:
-                message.append("{}\t{}\t<{}>".format(*game.values()))
-            message.append(category_separator)
+                line = "{}\t{}\t<{}>".format(*game.values())
+                mlines.append(line)
+        
+        # group messages
+        max_length = 1500
+        sub_total = 0
+        for line in mlines:
+            sub_total += len(line) + 1
+            partial_message += line + '\n'
             
-        self.message = message
+            if sub_total > max_length:
+                sub_total = 0
+                full_message.append(partial_message)
+                partial_message = ''
+        
+        self.message = full_message
     
     
     def write_to_json(self):
